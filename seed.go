a15ccc870f32539b58110efc953d16c3d21509cc
Patrick McNamara <hello@patrickmcnamara.xyz>
2021-02-05 16:39:08 +0000

    Use new seed.Copy function

--- seed.go
+++ seed.go
@@ -9,12 +9,6 @@ import (
 func seedToText(sr *seed.Reader) string {
 	var sb strings.Builder
 	sw := seed.NewWriter(&sb)
-	for {
-		line, err := sr.ReadLine()
-		if err != nil {
-			break
-		}
-		sw.Line(line)
-	}
+	seed.Copy(sw, sr)
 	return sb.String()
 }
